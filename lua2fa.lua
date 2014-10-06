587f4108194ef1d79e8767af099be32cce02e7db
Thejesh GN <i@thejeshgn.com>
2014-10-06 20:46:00 +0530

    Create lua2fa.lua
    initial file

---
+++ lua2fa.lua
@@ -0,0 +1,13 @@
+--[[
+# lua2fa.lua -- version 0.1
+
+<https://github.com/thejeshgn/lua2fa>
+
+**Author:** Thejesh GN, <i@thejeshgn.com>  
+**Date:** 2014
+**License:** GNU GPL version 3.
+
+
+
+
+]] 
